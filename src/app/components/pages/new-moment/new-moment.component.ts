d3acd18ed480c5a8e41138796c03b96014f458c5
Miguel Brack <miguel.brack@meisters.solutions>
2023-06-21 08:28:44 -0300

    My moment

---
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -0,0 +1,40 @@
+import { Component } from '@angular/core';
+
+import { Moment } from 'src/app/Moment';
+import { Router } from '@angular/router';
+
+import { MomentService } from 'src/app/services/moment.service';
+
+import { MessagesService } from 'src/app/services/messages.service';
+
+@Component({
+  selector: 'app-new-moment',
+  templateUrl: './new-moment.component.html',
+  styleUrls: ['./new-moment.component.css'],
+})
+export class NewMomentComponent {
+  btnText = 'Enviar';
+
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router
+  ) {}
+
+  async createHandler(moment: Moment) {
+    const formData = new FormData();
+
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
+
+    if (moment.image) {
+      formData.append('image', moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add('Momento adicionado com sucesso');
+
+    this.router.navigate(['/']);
+  }
+}
